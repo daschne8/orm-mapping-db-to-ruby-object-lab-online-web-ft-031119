0c79238a9641fca2c7e8147dfcd2ccceb242ae4c
Daniel Schneider <daschne8@gmail.com>
2019-04-10 17:41:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,7 +77,7 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE grade = 10 LIMIT 1
     SQL
-    DB[:conn].execute(sql,x).map { |row| self.new_from_db(row) }
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
   end
 
 end
