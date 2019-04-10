c61355191ecbca5848a4240617b57e7030ae1e90
Daniel Schneider <daschne8@gmail.com>
2019-04-10 17:44:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,4 +80,11 @@ class Student
     self.new_from_db(DB[:conn].execute(sql)[0])
   end
 
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql,x).map { |row| self.new_from_db(row) }
+  end
+
 end
